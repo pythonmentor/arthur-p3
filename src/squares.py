13a63fdad75f3832f98233c0ff4cda11981233ca
Thierry Chappuis <tchappui@users.noreply.github.com>
2019-11-24 10:05:44 +0100

    Commentaires square.py

--- src/squares.py
+++ src/squares.py
@@ -7,9 +7,11 @@ class Square(object):
     def __init__(self, coords):
         self.coords = coords
 
+    # -tc- can_move
     def canMove(self):
         return False
 
+    # -tc- que fait after_move
     def afterMove(self, gyver):
         return 0
 
@@ -17,6 +19,7 @@ class Wall(Square):
     """docstring for Wall."""
 
     def __init__(self, coords):
+        # -tc- super().__init__(coords)
         super(Wall, self).__init__(coords)
         self.type = "Wall"
 
@@ -24,8 +27,10 @@ class Guard(Square):
     """docstring for Guard."""
 
     def __init__(self, coords):
+        # -tc- super().__init__(coords)
         super(Guard, self).__init__(coords)
         self.type = "Guard"
 
+    # -tc- Dans Square, after_move retourne une valeur
     def afterMove(self, gyver):
         gyver.win()
