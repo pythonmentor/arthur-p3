048ddb3bda1d6ce27c9a97ac67f532e30124da2c
Thierry Chappuis <tchappui@users.noreply.github.com>
2019-11-24 09:47:24 +0100

    Commentaires gyver.py

--- src/gyver.py
+++ src/gyver.py
@@ -1,11 +1,14 @@
 class Gyver(object):
     """docstring for Gyver."""
 
+    # -tc- Attention, en python, ça déclare des
+    # -tc- attributs de classe, pas d'instance.
     coords = []
     deck = 0
     life = -1
 
     def __init__(self, coords, **kwargs):
+        # -tc- Inutile ici, mais la syntaxe correcte est super().__init__()
         super(Gyver, self).__init__()
         self.coords = coords
         self.deck = 1
