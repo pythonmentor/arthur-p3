51e1c034d8e37fd9ec29ed309c25de5cba2d23f3
Thierry Chappuis <tchappui@users.noreply.github.com>
2019-11-24 12:36:47 +0100

    Commented gameloop.py

--- src/gameloop.py
+++ src/gameloop.py
@@ -1,19 +1,23 @@
 class GameLoop(object):
     """docstring for GameLoop."""
 
+    # -tc- inutile, pas utilisé
     gyver = None
     labyrinth = None
 
     def __init__(self, gyver, labyrinth, **kwargs):
+        # -tc- inutile
         super(GameLoop, self).__init__()
         self.gyver = gyver
         self.labyrinth = labyrinth
 
         self.printGame()
 
+    # -tc- attention aux conventions de nommage: voir PEP8
     def printGame(self):
         maps = self.labyrinth.maps
 
+        # -tc- pour une meilleure visualisation en terminal, afficher une seule lettre par case
         for row in maps:
             s = ''
             for square in row:
