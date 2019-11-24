9d39265f865611571d243873ba42689784ab5587
Thierry Chappuis <tchappui@users.noreply.github.com>
2019-11-24 09:55:37 +0100

    Commentaires main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,23 @@
-#!venv/bin/python3.5
+#!venv/bin/python3.5 # -tc- je ne compterais pas sur le shebang pour le lancement ici
 
-import os
+import os # -tc- ajouter une ligne vide après les imports standards
 from gyver import Gyver
 from labyrinth import Labyrinth
 from gameloop import GameLoop
 
+# -tc- Si tu lances le programme correctement, le
+# -tc- ROOT_DIR sera le répertoire courant. Dans le doute,
+# -tc- tu peux utiliser os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# -tc- pathlib permet une syntaxe plus clair et est le module recommandé
 ROOT_DIR = os.path.abspath('..')
 RES_DIR = ROOT_DIR + '/res'
 
 gyver = Gyver(coords=[1, 2])
 labyrinth = Labyrinth()
 
+# -tc- le fichier est habituellement appelé __main__.py, mais le point
+# -tc- d'entrée est généralement appelé main()
 def __main__():
     gameLoop = GameLoop(gyver, labyrinth)
     #gameLoop.startLoop()
