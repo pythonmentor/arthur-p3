f1b2c83573e20a9d72b1dfe44ff6d9fabf723d58
Thierry Chappuis <tchappui@users.noreply.github.com>
2019-11-24 08:48:09 +0100

    Added comments in play.sh

--- play.sh
+++ play.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# -tc- Pas portable, éviter de lancer un scipt python avec un script bash. D'autant que ce 
+# -tc- n'est pas nécessaire. Un script python aussi peut avoir un shebang
+
 ./src/main.py
